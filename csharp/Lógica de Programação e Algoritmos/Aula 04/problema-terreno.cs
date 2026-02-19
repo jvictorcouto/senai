e367be0fa5a20c8a1bf89e8cb5dea25fefc19beb
joaocoutosenai-backend <joaocouto.senai@gmail.com>
2026-02-18 21:43:08 -0800

    Enhance terreno.cs output formatting and clarity; updated area and price messages for better user understanding.

--- csharp/Lógica de Programação e Algoritmos/Aula 04/problema-terreno.cs
+++ csharp/Lógica de Programação e Algoritmos/Aula 04/problema-terreno.cs
@@ -7,7 +7,9 @@ WriteLine("Digite o comprimento do terreno:");
 comprimento = double.Parse(ReadLine());
 WriteLine("Digite o valor do m2 do terreno:");
 valormetroquadrado = double.Parse(ReadLine());
+//---Processamento de dados----
 area = largura * comprimento;
 preco = area * valormetroquadrado;
-WriteLine("A área do terreno é: " + area);
-WriteLine("O preço do terreno é: " + preco);
+//----Saída de dados----    
+WriteLine("A área do terreno em metros quadrados é de: " + area);
+WriteLine("O preço do terreno é de R$: " + preco);
